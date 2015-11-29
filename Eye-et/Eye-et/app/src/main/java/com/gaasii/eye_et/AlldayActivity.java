f619ea53086d72135629ccc917c04cb744756ec2
Toshiyuki Ando <gaasii@gaasii.com>
2015-11-29 12:41:37 +0900

    [スマホ]画面遷移

--- Eye-et/Eye-et/app/src/main/java/com/gaasii/eye_et/AlldayActivity.java
+++ Eye-et/Eye-et/app/src/main/java/com/gaasii/eye_et/AlldayActivity.java
@@ -6,17 +6,16 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
-
-public class MainActivity extends AppCompatActivity {
+public class AlldayActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_allday);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -46,6 +45,8 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_mydata) {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
             return true;
         }else if (id == R.id.action_today) {
             Intent intent = new Intent(this, TodayActivity.class);
@@ -57,10 +58,11 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }else if (id == R.id.action_settings) {
             Intent intent = new Intent(this, SettingActivity.class);
-            startActivity(intent);
+            //startActivity(intent);
             return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
+
 }
--- Eye-et/Eye-et/app/src/main/java/com/gaasii/eye_et/SettingActivity.java
+++ Eye-et/Eye-et/app/src/main/java/com/gaasii/eye_et/SettingActivity.java
@@ -6,17 +6,16 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
-
-public class MainActivity extends AppCompatActivity {
+public class SettingActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_setting);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -46,6 +45,7 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_mydata) {
+            Intent intent = new Intent(this, MainActivity.class);
             return true;
         }else if (id == R.id.action_today) {
             Intent intent = new Intent(this, TodayActivity.class);
@@ -56,11 +56,12 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intent);
             return true;
         }else if (id == R.id.action_settings) {
-            Intent intent = new Intent(this, SettingActivity.class);
-            startActivity(intent);
+            //Intent intent = new Intent(this, SettingActivity.class);
+            //startActivity(intent);
             return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
+
 }
